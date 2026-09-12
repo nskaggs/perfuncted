204caae7ea9c3db7c311b34778880f44dc52cc25
Nicholas Skaggs <nskaggs@gmail.com>
2026-09-11 21:58:34 -0400

    fix: try only QT_ACCESSIBILITY=1 for kwrite certification
    QT_LINUX_ACCESSIBILITY_ALWAYS_ON=1 seems to cause AT-SPI bus
    disconnect on CI. Try with just QT_ACCESSIBILITY=1.

--- integration/atspi_certification_test.go
+++ integration/atspi_certification_test.go
@@ -61,7 +61,7 @@ func TestAccessibilityCertification(t *testing.T) {
 			name:     "kwrite",
 			launch:   []string{"kwrite"},
 			winMatch: "kwrite",
-			extraEnv: []string{"QT_ACCESSIBILITY=1", "QT_LINUX_ACCESSIBILITY_ALWAYS_ON=1"},
+			extraEnv: []string{"QT_ACCESSIBILITY=1"},
 		},
 		{
 			name:     "gnome-text-editor",
